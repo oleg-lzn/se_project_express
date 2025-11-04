718616bcbc8502649682e525f2e3c1014c989b52
Oleg <luzeninoleg2012@gmail.com>
2025-11-04 13:46:18 +0200

    fixed the standart value for url

--- utils/config.js
+++ utils/config.js
@@ -5,12 +5,7 @@ const randomString = crypto
   .randomBytes(32) // generating a random sequence of 16 bytes (128 bits)
   .toString("hex"); // converting it into a string
 
-const {
-  JWT_SECRET = "CAT_FISH",
-  // DB_URL = "mongodb://127.0.0.1:27017/wtwr_db",
-  DB_URL = "mongodb+srv://luzeninoleg2012_db_user:uspPJPrQkbFkXvnu@wtwr.nsoayrq.mongodb.net/?appName=WTWR",
-  PORT = 3001,
-} = process.env;
+const { JWT_SECRET, DB_URL, PORT } = process.env;
 
 module.exports = {
   JWT_SECRET,
